de557ea45d0349d2ebda50447cac5318cfeb7081
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2026-02-15 20:59:02 -0800

    chore: update formatting to align with Python style guide

--- wikiTraverse.py
+++ wikiTraverse.py
@@ -16,77 +16,82 @@ DEFAULT_STEP_LIMIT = 10
 
 nlp = spacy.load("en_core_web_lg")
 
+
 def url_to_title(url):
-  return url.rsplit('/', 1)[-1].replace("_", " ")
+    return url.rsplit("/", 1)[-1].replace("_", " ")
+
 
 def extract_article_url(href):
-  # Strip any in-page anchor (#Section)
-  clean_href = href.split("#")[0]
-  return WIKIPEDIA_BASE_URL + clean_href
+    # Strip any in-page anchor (#Section)
+    clean_href = href.split("#")[0]
+    return WIKIPEDIA_BASE_URL + clean_href
+
 
 def traverseWiki(startURL, targetURL, limit=DEFAULT_STEP_LIMIT):
-  # Parse the targetURL for the semantic meaning of the title
-  targetTitle = url_to_title(targetURL)
-  targetSemantic = nlp(targetTitle)
-  currentURL = startURL
-  # Path keeps track of the pages we ultimately visit
-  path = [currentURL]
+    # Parse the targetURL for the semantic meaning of the title
+    targetTitle = url_to_title(targetURL)
+    targetSemantic = nlp(targetTitle)
+    currentURL = startURL
+    # Path keeps track of the pages we ultimately visit
+    path = [currentURL]
 
-  for i in range(limit):
-    # 1. Determine if current URL is the target URL
-    if currentURL == targetURL:
-      print("Success!")
-      print(path)
-      return 1
-    
-    # 2. Go to valid URL
-    response = requests.get(
-      url=currentURL,
-    )
-    soup = BeautifulSoup(response.content, 'html.parser')
+    for i in range(limit):
+        # 1. Determine if current URL is the target URL
+        if currentURL == targetURL:
+            print("Success!")
+            print(path)
+            return 1
 
-    # 3a. Collect links
-    allLinks = soup.find(id="bodyContent").find_all("a")
-    semanticSimilarity = 0
+        # 2. Go to valid URL
+        response = requests.get(
+            url=currentURL,
+        )
+        soup = BeautifulSoup(response.content, "html.parser")
 
-    for link in allLinks:
-      # 3b. Sort out the 'bad' links
-      # (non-Wiki references, non-main articles)
-      linkHref = link.get('href', "")
-      if linkHref.find(WIKIPEDIA_ARTICLE_PREFIX) == -1:
-        continue
-      if ((linkHref.find("/Special:") != -1) or 
-          (linkHref.find("/Talk:") != -1) or 
-          (linkHref.find("/Category:") != -1) or 
-          (linkHref.find("/File:") != -1) or
-          (linkHref.find("/Wikipedia:") != -1) or
-          (linkHref.find("/Template:") != -1) or
-          (linkHref.find("wikidata") != -1) or
-          (linkHref.find("/Help:") != -1)):
-        continue
-      linkURL = extract_article_url(linkHref)
-      # 3c. In order to prevent loops, I prevent the
-      # program from looking at links to pages
-      # we've already traversed
-      if linkURL in path:
-        continue
-      # 4. Parsing links for article titles
-      currentTitle = linkURL.rsplit('/', 1)[-1].replace("_", " ")
-      # 5a. Run a semantic comparison on each article title with the target article title
-      currentSemantic = nlp(currentTitle)
-      similarity = targetSemantic.similarity(currentSemantic)
-      # 5b/6. Keep track (and eventually go to) the page with the highest semantic similarity
-      if similarity > semanticSimilarity:
-        print("Most similar changed to " + currentTitle)
-        semanticSimilarity = similarity
-        currentURL = linkURL
-    path.append(currentURL)
-    sleep(1)
+        # 3a. Collect links
+        allLinks = soup.find(id="bodyContent").find_all("a")
+        semanticSimilarity = 0
 
+        for link in allLinks:
+            # 3b. Sort out the 'bad' links
+            # (non-Wiki references, non-main articles)
+            linkHref = link.get("href", "")
+            if linkHref.find(WIKIPEDIA_ARTICLE_PREFIX) == -1:
+                continue
+            if (
+                (linkHref.find("/Special:") != -1)
+                or (linkHref.find("/Talk:") != -1)
+                or (linkHref.find("/Category:") != -1)
+                or (linkHref.find("/File:") != -1)
+                or (linkHref.find("/Wikipedia:") != -1)
+                or (linkHref.find("/Template:") != -1)
+                or (linkHref.find("wikidata") != -1)
+                or (linkHref.find("/Help:") != -1)
+            ):
+                continue
+            linkURL = extract_article_url(linkHref)
+            # 3c. In order to prevent loops, I prevent the
+            # program from looking at links to pages
+            # we've already traversed
+            if linkURL in path:
+                continue
+            # 4. Parsing links for article titles
+            currentTitle = linkURL.rsplit("/", 1)[-1].replace("_", " ")
+            # 5a. Run a semantic comparison on each article title with the target article title
+            currentSemantic = nlp(currentTitle)
+            similarity = targetSemantic.similarity(currentSemantic)
+            # 5b/6. Keep track (and eventually go to) the page with the highest semantic similarity
+            if similarity > semanticSimilarity:
+                print("Most similar changed to " + currentTitle)
+                semanticSimilarity = similarity
+                currentURL = linkURL
+        path.append(currentURL)
+        sleep(1)
 
-  print("Failure! Traversal limit exceeded!")
-  print(path)
-  return 0
+    print("Failure! Traversal limit exceeded!")
+    print(path)
+    return 0
+
 
 # Example of use:
 # traverseWiki("https://en.wikipedia.org/wiki/Big_Bang", "https://en.wikipedia.org/wiki/Taylor_Swift", 10)
