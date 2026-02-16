3386adb0f4bbf8f4e9330402789be8ecc2523814
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2026-02-15 21:49:35 -0800

    refactor: make article link extraction code only consider links to other articles, generate a unique set of links per article, and move code to a separate function

--- wikiTraverse.py
+++ wikiTraverse.py
@@ -49,9 +49,30 @@ def extract_article_url(href):
     clean_href = href.split("#")[0]
     return WIKIPEDIA_BASE_URL + clean_href
 
+
 def fetch_page(url):
     response = requests.get(url)
     return BeautifulSoup(response.content, "html.parser")
+
+
+def extract_article_links(soup):
+    body = soup.find(id="bodyContent")
+    if body is None:
+        return []
+
+    seen = set()
+    links = []
+    # Iterate through all links with an href (link to another article)
+    for tag in body.find_all("a", href=True):
+        href = tag["href"]
+        if not is_valid_wiki_link(href):
+            continue
+        url = extract_article_url(href)
+        # Filter out non-unique links
+        if url not in seen:
+            seen.add(url)
+            links.append(url)
+    return links
 
 
 def traverseWiki(startURL, targetURL, limit=DEFAULT_STEP_LIMIT):
@@ -73,31 +94,25 @@ def traverseWiki(startURL, targetURL, limit=DEFAULT_STEP_LIMIT):
         soup = fetch_page(currentURL)
 
         # 3a. Collect links
-        allLinks = soup.find(id="bodyContent").find_all("a")
+        links = extract_article_links(soup)
         semanticSimilarity = 0
 
-        for link in allLinks:
-            # 3b. Sort out the 'bad' links
-            # (non-Wiki references, non-main articles)
-            linkHref = link.get("href", "")
-            if not is_valid_wiki_link(linkHref):
-                continue
-            linkURL = extract_article_url(linkHref)
-            # 3c. In order to prevent loops, I prevent the
+        for link in links:
+            # 3b. In order to prevent loops, I prevent the
             # program from looking at links to pages
             # we've already traversed
-            if linkURL in path:
+            if link in path:
                 continue
             # 4. Parsing links for article titles
-            currentTitle = linkURL.rsplit("/", 1)[-1].replace("_", " ")
+            current_title = url_to_title(link)
             # 5a. Run a semantic comparison on each article title with the target article title
-            currentSemantic = nlp(currentTitle)
+            currentSemantic = nlp(current_title)
             similarity = targetSemantic.similarity(currentSemantic)
             # 5b/6. Keep track (and eventually go to) the page with the highest semantic similarity
             if similarity > semanticSimilarity:
-                print("Most similar changed to " + currentTitle)
+                print("Most similar changed to " + current_title)
                 semanticSimilarity = similarity
-                currentURL = linkURL
+                currentURL = link
         path.append(currentURL)
         sleep(1)
 
